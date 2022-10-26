62c3f913f05b0514690bb9872bc854b8e894949d
adeneye02 <tosinolatunbosun02@gmail.com>
2022-10-26 15:00:17 -0700

    task 9 commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,32 @@
 #include "main.h"
 
 /**
- *print_number - Prints any integer with putchar
- *@n: Number to prins
- *Return: Nothing
+ *print_number - print numbers chars
+ *@n: integer params
+ *Return: 0
  */
+
+
 
 void print_number(int n)
 
 {
 
-		unsigned int x;
+	unsigned int n1;
 
-		if (n < 0)
-		{
-		_putchar('-');
+	n1 = n;
 
-		n *= -1;
+	if (n < 0)
+	{
+	_putchar('-');
 
-		}
-		x = n;
+	n1 = -n;
 
-		if (x / 10)
-		print_number(x / 10);
+	}
+	if (n1 / 10 != 0)
+	{
+	print_number(n1 / 10);
 
-		_putchar(x % 10 + '0');
-
+	}
+	_putchar((n1 % 10) + '0');
 }
