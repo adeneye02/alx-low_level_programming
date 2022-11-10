92be93def6e314c2ec08bfbfc3c09516ac31790c
adeneye02 <tosinolatunbosun02@gmail.com>
2022-11-10 10:53:59 -0800

    task 2 commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,47 +1,31 @@
-#include <stdlib.h>
-
 #include "main.h"
 
 /**
- * _calloc - function that allocates memory for an array, using malloc
- * @nmemb: input size 1
- * @size: input size bytes
- * Return: nothing
+ * _calloc - allocate memory using malloc and initialize it to zero
+ * @nmemb: number of elements
+ * @size: size of the memory block to be allocated
+ * Return: poiner to the address of the memory block
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 
 {
 
-		void *cal;
+		char *block;
+
+		unsigned int i;
 
 		if (nmemb == 0 || size == 0)
 		return (NULL);
-		cal = malloc(nmemb * size);
-		if (cal == NULL)
-		return (NULL);
-		_memset(cal, 0, nmemb * size);
-		return (cal);
-}
 
-/**
- * _memset - function that fills memory with a constant byte.
- * @s: mamory area to return
- * @b: constant byte
- * @n: size of bytes
- * Return: char
- */
-
-char *_memset(char *s, char b, unsigned int n)
-
-{
-
-		unsigned int i = 0;
-
-		while (i < n)
+		block = malloc(nmemb * size);
+		if (block != NULL)
 		{
-		*(s + i) = b;
-		i += 1;
+		for (i = 0; i < (nmemb * size); i++)
+		block[i] = 0;
+		return (block);
 		}
-		return (s);
+		else
+		return (NULL);
+
 }
